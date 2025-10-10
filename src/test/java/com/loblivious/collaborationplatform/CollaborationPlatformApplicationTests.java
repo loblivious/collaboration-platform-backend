52b0a6baa5ceed84bc19894065a65ac67a834934
Yen-Jung Luo <loblivious.ee@gmail.com>
2025-10-10 18:08:10 +0800

    project init

---
+++ src/test/java/com/loblivious/collaborationplatform/CollaborationPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loblivious.collaborationplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollaborationPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
