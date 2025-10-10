c37cc89def24c1516dc221d4ec7014ef4ca48705
Yen-Jung Luo <loblivious.ee@gmail.com>
2025-10-10 23:00:49 +0800

    set up db; created user entity, repository, and security config

---
+++ src/main/java/com/loblivious/collaborationplatform/domain/User.java
@@ -0,0 +1,29 @@
+package com.loblivious.collaborationplatform.domain;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String passwordHash;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+}
